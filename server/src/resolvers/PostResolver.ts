5ac919fb8efab5267db68b7c1081e0428de9bdc4
Lea Suc <lea.suc5@gmail.com>
2026-07-17 18:21:48 +0200

    fix: delete post from upvote table first

--- server/src/resolvers/PostResolver.ts
+++ server/src/resolvers/PostResolver.ts
@@ -201,6 +201,18 @@ export class PostResolver {
     @Arg("id", () => Int) id: number,
     @Ctx() { req }: MyContext,
   ): Promise<boolean> {
+    const post = await Post.findOne({
+      where: {
+        id,
+      },
+    });
+    if (!post) {
+      return false;
+    }
+    if (post?.creatorId !== req.session.userId) {
+      throw new Error("Not authorized");
+    }
+    await Upvote.delete({ postId: id });
     await Post.delete({ id, creatorId: req.session.userId });
     return true;
   }
